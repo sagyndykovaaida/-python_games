7ac620537f33ec685ba64af90efece6e43e18cd2
kairbayeva <dkairbaeva03@gmail.com>
2023-04-04 08:56:03 +0600

    a fuctions that returns a list,tuple,dist

---
+++ list.py
@@ -0,0 +1,13 @@
+def get_list(n):
+    squares = []
+    for i in range(1, n+1):
+        squares.append(i**2)
+    return squares
+
+def get_tuple(a, b, c):
+    return (a, b, c)
+
+def get_dict():
+    fruits = {"apple": "red", "banana": "yellow", "orange": "orange"}
+    return fruits
+
--- nonfunc.py
+++ nonfunc.py
@@ -3,3 +3,7 @@ def functional(resume):
     required_keywords = ["Python", "machine learning", "data analysis", "problem solving"]
     return all(keyword in resume.lower() for keyword in required_keywords)
 
+def non_functional(resume):
+   
+    print("This candidate has a degree in Computer Science and 3 years of experience in software development.")
+    print("They are proficient in Python, Java, and C++, and have worked on several projects involving machine learning and data analysis.")
