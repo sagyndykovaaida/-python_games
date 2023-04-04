e12ec4bded7938b40a5fe31a32cadae7838d4112
kairbayeva <dkairbaeva03@gmail.com>
2023-04-04 09:06:05 +0600

    add function result_reduce

--- map.py
+++ map.py
@@ -1,4 +1,4 @@
-# from functools import reduce
+from functools import reduce
 
 my_list = [1, 2, 3, 4, 5]
 result_map = list(map(lambda x: x * 2, my_list))
@@ -6,5 +6,8 @@ print(result_map)
 
 result_filter = list(filter(lambda x: x % 2 == 0, my_list))
 print(result_filter) 
+
+result_reduce = reduce(lambda x, y: x * y, my_list)
+print(result_reduce)
 
 
