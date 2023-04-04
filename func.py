1acfcebc259c0853f4f1bef336798f573c995a03
sagyndykovaaida <lvnidass@gmail.com>
2023-04-04 08:32:06 +0600

    change file func.py

--- func.py
+++ func.py
@@ -3,5 +3,17 @@ def compose_functions(f, g):
         return f(g(x))
     return h
 
+def add_one(x):
+    return x + 1
 
-# print(h(5))  
+def multiply(x):
+    return 2 * x
+
+f = input("first function (e.g., add_one): ")
+g = input("second function (e.g., multiply): ")
+
+f = globals()[f]
+g = globals()[g]
+
+h = compose_functions(f, g)
+print(h(5))  
